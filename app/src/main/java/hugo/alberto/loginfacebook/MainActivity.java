50e609de85834c30be9d23077a489d63fe273b41
albertohugo <albertohugo@gmail.com>
2016-06-10 15:31:01 +0100

    Get some informations from facebook

--- app/src/main/java/hugo/alberto/loginfacebook/MainActivity.java
+++ app/src/main/java/hugo/alberto/loginfacebook/MainActivity.java
@@ -1,34 +1,50 @@
 package hugo.alberto.loginfacebook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    CallbackManager callbackManager= CallbackManager.Factory.create();
+    CallbackManager callbackManager;
+    private ProfilePictureView profilepic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_main);
-
+        callbackManager = CallbackManager.Factory.create();
         AppEventsLogger.activateApp(this);
 
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("user_friends");
+
+        loginButton.setReadPermissions(Arrays.asList("user_friends", "email", "public_profile","user_birthday"));
+        profilepic = (ProfilePictureView) findViewById(R.id.picture);
 
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                // App code
+                setFacebookData(loginResult);
             }
 
             @Override
@@ -41,5 +57,67 @@ public class MainActivity extends AppCompatActivity {
                 // App code
             }
         });
+    }
+
+    private void setFacebookData(final LoginResult loginResult)
+    {
+        GraphRequest request = GraphRequest.newMeRequest(
+                loginResult.getAccessToken(),
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                            Log.i("Response",response.toString());
+
+                            String email = response.getJSONObject().getString("email");
+                            String firstName = response.getJSONObject().getString("first_name");
+                            String lastName = response.getJSONObject().getString("last_name");
+                            String gender = response.getJSONObject().getString("gender");
+                            String bday= response.getJSONObject().getString("birthday");
+
+                            Profile profile = Profile.getCurrentProfile();
+                            String id = profile.getId();
+                            String link = profile.getLinkUri().toString();
+                            Log.i("Link",link);
+                            if (Profile.getCurrentProfile()!=null)
+                            {
+                                Log.i("Login", "ProfilePic" + Profile.getCurrentProfile().getProfilePictureUri(200, 200));
+                            }
+
+                            TextView email_txt = (TextView) findViewById(R.id.email);
+                            email_txt.setText(email);
+
+                            TextView name_txt = (TextView) findViewById(R.id.name);
+                            name_txt.setText(firstName + " " + lastName);
+
+                            TextView gender_txt = (TextView) findViewById(R.id.gender);
+                            gender_txt.setText(gender);
+
+                            TextView bday_txt = (TextView) findViewById(R.id.bday);
+                            bday_txt.setText(bday);
+
+                            profilepic.setProfileId(id);
+
+                            Log.i("Login" + "Email", email);
+                            Log.i("Login"+ "FirstName", firstName);
+                            Log.i("Login" + "LastName", lastName);
+                            Log.i("Login" + "Gender", gender);
+                            Log.i("Login" + "Bday", bday);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,email,first_name,last_name,gender, birthday");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
